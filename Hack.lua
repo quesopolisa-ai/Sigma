371ed7c45ea109e6eba15b67f0da9b27df5313df
quesopolisa-ai <quesopolisa@gmail.com>
2026-05-29 15:13:51 -0400

    Update Hack.lua

--- Hack.lua
+++ Hack.lua
@@ -1,7 +1,7 @@
 -- ==========================================
 -- CONFIGURATION
 -- ==========================================
-local PART_NAME = "str14ic" -- CHANGE THIS to the exact name of the part!
+local PART_NAME = "str15ic" -- CHANGE THIS to the exact name of the part!
 
 -- ==========================================
 -- 1. RESET ONCE AT THE START
